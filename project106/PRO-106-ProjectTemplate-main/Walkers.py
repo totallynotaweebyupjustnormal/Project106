20158c84c903d2701e4cc743a40505b1a41bc793
JustYourAverageAo3andWattpadReader <106915907+totallynotaweebyupjustnormal@users.noreply.github.com>
2023-12-26 18:08:26 +0800

    Walkers.py

--- project106/PRO-106-ProjectTemplate-main/Walkers.py
+++ project106/PRO-106-ProjectTemplate-main/Walkers.py
@@ -1,28 +1,28 @@
 import cv2
 
+body_classifier = cv2.CascadeClassifier('C:\Users\CRISILJEFV GARRIDO\Downloads\project106\PRO-106-ProjectTemplate-main')
 
-# Create our body classifier
-
-
-# Initiate video capture for video file
 cap = cv2.VideoCapture('walking.avi')
 
-# Loop once video is successfully loaded
 while True:
     
-    # Read first frame
     ret, frame = cap.read()
 
-    #Convert Each Frame into Grayscale
-    
-    # Pass frame to our body classifier
-    
-    
-    # Extract bounding boxes for any bodies identified
-    
+    if not ret:
+        break
 
-    if cv2.waitKey(1) == 32: #32 is the Space Key
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    bodies = body_classifier.detectMultiScale(gray, 1.1, 3)
+    
+    for (x, y, w, h) in bodies:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Pedestrians', frame)
+
+    if cv2.waitKey(1) == 32:
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
